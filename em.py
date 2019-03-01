9e61cc7ec3c0605107ea449152788c5adbb6a340
Michal Lisicki <michallisicki@gmail.com>
2019-02-28 19:40:24 -0500

    Fully implemented. It spits out some numbers, but they don't look right. It's time to debug

--- em.py
+++ em.py
@@ -7,7 +7,7 @@ def poisson_pdf(x,lmbd):
     '''
     Returns the Poisson probability of each given sample
     '''
-    return math.exp(-lmbd)*lmbd**x/math.factorial(x)
+    return np.array([math.exp(-lmbd)*lmbd**_x/math.factorial(_x) for _x in x])
 
 def poisson_random(lmbd, numsamples=1):
     '''
@@ -22,20 +22,14 @@ def zip_pdf(x,pi,lmbd):
     '''
     Returns the ZIP probability of each given sample
     '''
-    return [poisson_pdf(x)+pi*(x>0)]
+    return poisson_pdf(x,lmbd)+pi*(x>0)
 
 def zip_random(lmbd, pi, numsamples=1):
     '''
     Draws samples from a zero-inflated Poisson (ZIP) distribution
     '''
-    return [0 if random.random()<pi else poisson_random(lmbd)[0] \
-            for _ in range(numsamples)]
-
-#sample = (zip_random(3,0.3,10000))
-#bins = np.arange(0, max(sample) + 1.5) - 0.5
-#plt.hist(sample,bins)
-#   plt.show()
-
+    return np.array([0 if random.random()<pi else poisson_random(lmbd)[0] \
+            for _ in range(numsamples)])
 
 class ZIP_EM(object):
     '''
@@ -51,9 +45,7 @@ class ZIP_EM(object):
         which are needed in the next step.
         '''
         E_zi = np.divide(pi*(x>0),
-                         np.multiply(pi*(x>0),
-                                     (1-pi)*poisson_pdf(x,lmbd)
-                                    )
+                         pi*(x>0) + (1-pi)*poisson_pdf(x,lmbd)
                         )
         return E_zi
 
@@ -66,37 +58,45 @@ class ZIP_EM(object):
         lmbd = np.sum(np.multiply(1-E_zi,x))/np.sum(1-E_zi)
         return pi, lmbd
 
-    def stop(self,x):
+    def stop(self, x, pi, lmbd):
         '''
         Implementation of the AAC stopping criterion
         '''
-        next_likelihood = np.sum(np.log(zip_pdf(x)))
+        next_likelihood = np.sum(np.log(zip_pdf(x, pi, lmbd)))
         self.error = np.abs(next_likelihood - self.likelihood)
         self.likelihood = next_likelihood
         return self.error < self.rho
 
-    def run(self, sample, init_pi=0.5, init_lambda=None, rho=1e-4):
+    def run(self, x, init_pi=0.5, init_lmbd=None, rho=1e-4):
         '''
         Runs the EM algorithm
-        :param sample: A vector of samples from ZIP distribution.
+        :param x: A vector of samples from ZIP distribution.
         :param init_pi: Initial value for pi. If not provided the default value
         0.5 will be used
-        :param init_lambda: Initial value for Poisson lambda parameter. If not
+        :param init_lmbd: Initial value for Poisson lmbd parameter. If not
         provided the sample mean will be used
         :param rho: Stopping criterion tolerance.
         '''
-        headers = ["Iter", "Pi", "Lambda", "l", "Error"]
-        print("{:>5}"*len(headers).format(headers))
+        self.lmbd = init_lmbd if init_lmbd is not None else np.mean(x)
+        self.pi = init_pi
+        self.rho = rho
+        self.likelihood = np.inf
+        headers = ["Iter", "Pi", "lmbd", "l", "Error"]
+        print(("{:>15}"*len(headers)).format(*headers))
         i = 1
-        while not self.stop():
-            E_zi = self.estep(self.pi, self.lambda)
-            self.pi, self.lambda = self.mstep(E_zi)
-            print("{:>5}"*len(headers).format(i,self.pi,self.lambda,self.likelihood,self.error))
+        while not self.stop(x, self.pi, self.lmbd):
+            E_zi = self.estep(x, self.pi, self.lmbd)
+            self.pi, self.lmbd = self.mstep(x,E_zi)
+            print(("{:>15}"+"{:>15.5f}"*(len(headers)-1)).format(i,self.pi,self.lmbd,self.likelihood,self.error))
             i+=1
-        return self.pi, self.lambda
+        return self.pi, self.lmbd
 
 if __name__=="__main__":
-    solver = EM()
-    pi, lmbd = solver.run()
+    solver = ZIP_EM()
+    sample = zip_random(3,0.3,1000)
+    pi, lmbd = solver.run(sample)
 
-
+#sample = (zip_random(3,0.3,10000))
+#bins = np.arange(0, max(sample) + 1.5) - 0.5
+#plt.hist(sample,bins)
+#   plt.show()
